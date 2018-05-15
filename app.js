073e2d50acc0f6c27790c798b2b466b0a284b2a3
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-05-15 13:23:11 +0300

    add modal form for register button and add app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+// hide the articles
+
+$('#article1').hide();
+$('#article2').hide();
+$('#article3').hide();
+$('#article4').hide();
+$('#article5').hide();
+$('#article6').hide();
+
+// show the articles and hide them again
+
+$('#entireArticle1').mouseover(function(){
+	$('#article1').show();
+})
+$('#entireArticle1').mouseout(function(){
+	$('#article1').hide();
+})
+
+
+$('#entireArticle2').mouseover(function(){
+	$('#article2').show();
+})
+$('#entireArticle2').mouseout(function(){
+	$('#article2').hide();
+})
+
+
+$('#entireArticle3').mouseover(function(){
+	$('#article3').show();
+})
+$('#entireArticle3').mouseout(function(){
+	$('#article3').hide();
+})
+
+
+$('#entireArticle4').mouseover(function(){
+	$('#article4').show();
+})
+$('#entireArticle4').mouseout(function(){
+	$('#article4').hide();
+})
+
+
+$('#entireArticle5').mouseover(function(){
+	$('#article5').show();
+})
+$('#entireArticle5').mouseout(function(){
+	$('#article5').hide();
+})
+
+
+$('#entireArticle6').mouseover(function(){
+	$('#article6').show();
+})
+$('#entireArticle6').mouseout(function(){
+	$('#article6').hide();
+})
